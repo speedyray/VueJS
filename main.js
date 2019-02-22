c66cd473976675b5eb7fab1e8aa96df20aa32caf
Raynold Gyasi <speedyray2ray@gmail.com>
2019-02-22 14:04:34 -0500

    second commit

--- main.js
+++ main.js
@@ -4,7 +4,36 @@ var app = new Vue({
 	data: {
 		product: 'Socks',
 		image: './assets/vmSocks-green-onWhite.jpg',
-		inStock: false,
+		inStock: true,
+		details: ["80% cotton", "20% polyester", "Gender-neutral"],
 		// inventory:8
-	}
+		variants: [
+			{
+			  variantId:2234,
+			  variantColor: "green"
+		},
+		{     variantId: 2235,
+              variantColor: "blue"
+		}
+	
+	   ],
+
+	   sizes:[{
+		      sizeId: 2333,
+		      dimensions: "small:8-12",
+			 	 
+			},
+			{  
+			   sizeId: 2334,
+			   dimensions: "medium:19 - 23",
+			},
+			{   sizeId: 2335,
+				dimensions: "large:24- 28",
+				
+
+			},
+	
+	    ]
+	},
+
 })
